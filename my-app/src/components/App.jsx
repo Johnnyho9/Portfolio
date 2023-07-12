99d4db060c95c2d3a6f104a69fe4e20c0158dff5
Johnny <Johnnyho9293@gmail.com>
2023-07-11 23:44:40 -0700

    Add App component

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
 
 class App extends React.Component {
@@ -19,5 +17,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-
